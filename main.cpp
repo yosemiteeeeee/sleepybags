bc7e9e0b68d0b328350ba852d651247114e64985
Artemy <art.shilov.jr@gmail.com>
2024-05-04 03:17:07 +0500

    Выполненный 8 пункт

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "iterators.h"
 #include "fm.h"
 
+
 int main() {
     srand(static_cast<unsigned>(time(nullptr)));
 
@@ -14,12 +15,25 @@ int main() {
     KokonFactory kokonFactory;
     OdeyaloFactory odeyaloFactory;
 
-
     populateContainer(vecContainer, kokonFactory, rand() % 5 + 1);
     populateContainer(vecContainer, odeyaloFactory, rand() % 5 + 1);
 
+    VectorContainerIterator* iterator = new VectorContainerIterator(vecContainer.accessItems());
 
-    vecContainer.display();
+    // Декоратор для фильтрации элементов (выберем элементы с температурным рейтингом меньше 10)
+    FilterDecorator* filterDecorator = new FilterDecorator(iterator, [](SpMeshok* item) {
+        return item->getTemperatureRating() < 10;
+    });
+
+    OrderDecorator* orderDecorator = new OrderDecorator(filterDecorator);
+
+    while (orderDecorator->hasNext()) {
+        orderDecorator->next()->describe();
+    }
+
+    delete orderDecorator;
+    delete filterDecorator;
+    delete iterator;
 
     return 0;
 }
